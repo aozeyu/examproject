98ae326b40f2f4743040038cc75071170ffe0a3a
aozeyu <3484766443@qq.com>
2022-08-13 12:13:19 +0800

    :sparkles: 验证码接口✅

---
+++ examproject/README.md
@@ -0,0 +1,6 @@
+# 工程简介
+在线考试系统
+## 技术栈
+springboot + vue + mybatis + shiro
+# 延伸阅读
+
---
+++ examproject/src/test/java/com/example/examproject/ExamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
