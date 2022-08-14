94fd09c6fd8479553eafecb2b458e31996ae68f5
aozeyu <3484766443@qq.com>
2022-08-14 14:56:08 +0800

    :sparkles: 后端集成redis

--- examproject/src/main/java/com/example/examproject/Controller/AdminController.java
+++ examproject/src/main/java/com/example/examproject/Controller/AdminController.java
@@ -7,14 +7,20 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.examproject.Pojo.CommonResult;
 import com.example.examproject.Pojo.User;
 import com.example.examproject.Service.impl.UserServiceImpl;
+import com.example.examproject.Utils.RedisUtil;
+import com.example.examproject.Utils.SaltEncryption;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 /**
  * @program: examproject
@@ -30,7 +36,9 @@ import java.util.Objects;
 public class AdminController {
     @Autowired
     private UserServiceImpl userService;
-
+    @Autowired
+    private RedisUtil redisUtil;
+    ObjectMapper mapper = new ObjectMapper();
     @GetMapping("/getUser")
     @ApiOperation("获取用户信息,可分页 ----> 查询条件(可无)(username,trueName),必须有的(pageNo,pageSize)")
     public CommonResult<List<User>> getUser(
@@ -47,7 +55,7 @@ public class AdminController {
         if (!Objects.equals(trueName, "")) wrapper.like("true_name", trueName);
         userPage = userService.page(userPage, wrapper);
         List<User> users = userPage.getRecords();
-        return new CommonResult<List<User>>(200, "success", users);
+        return new CommonResult<>(200, "success", users);
     }
     @GetMapping("/handleUser/{type}")
     @ApiOperation("管理员操作用户: type=1(启用) 2(禁用) 3(删除) userIds(需要操作的用户id)")
@@ -76,5 +84,20 @@ public class AdminController {
             }
             return new CommonResult<>(200,"操作成功");
         }else return new CommonResult<>(233,"操作有误");
+    }
+
+    @PostMapping("/addUser")
+    @ApiOperation("管理员用户新增用户")
+    public CommonResult<String> addUser(@RequestBody User user) throws NoSuchAlgorithmException {
+        log.info("执行了===>AdminController中的addUser方法");
+        //盐值
+
+        String salt = UUID.randomUUID().toString().substring(0,6);
+        String newPwd= SaltEncryption.saltEncryption(user.getPassword(),salt);
+        user.setPassword(newPwd);
+        user.setSalt(salt);
+        user.setCreateDate(new Date());
+        boolean save = userService.save(user);
+        return save ? new CommonResult<>(200,"操作成功") : new CommonResult<>(233,"操作失败");
     }
 }
--- examproject/src/test/java/com/example/examproject/ExamprojectApplicationTests.java
+++ examproject/src/test/java/com/example/examproject/ExamprojectApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.examproject;
 
+import com.example.examproject.Pojo.User;
+import com.example.examproject.Utils.RedisUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class ExamprojectApplicationTests {
-
+    @Autowired
+    private RedisUtil redisUtil;
     @Test
-    void contextLoads() {
+    void testRedis() throws JsonProcessingException {
+        User user = new User(1, 1, "wzz", "112", "1231", "121", 1, new Date());
+        System.out.println(user);
+        ObjectMapper mapper = new ObjectMapper();
+        redisUtil.set("user:1", mapper.writeValueAsString(user),60);
+        System.out.println(mapper.readValue(redisUtil.get("user:1").toString(),User.class));
     }
 
 }
